44c422b2de36debd5a64c647ab025b504f66002d
Parshant Garg <garg17793@gmail.com>
2017-01-08 00:00:52 +0530

    Hibernate dependency set up

---
+++ HibernatePractice/src/main/java/garg/parshant/HibernatePractice/App.java
@@ -0,0 +1,13 @@
+package garg.parshant.HibernatePractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
